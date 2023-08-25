6a71075748be00476fb1c8b8a1f33cbb7a7441af
Andrei Agaev <andagaev@gmail.com>
2023-08-25 09:12:28 -0700

    add width and height, getters, setters

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,22 @@
 public class Main {
+    private int width;
+    private int height;
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
     public static void main(String[] args) {
     }
 }
